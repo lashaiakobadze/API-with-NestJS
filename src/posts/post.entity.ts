32ad920518519e5b1a2ab60ad780dab0ac578538
Lasha <lashaiakobadze98@gmail.com>
2023-09-28 18:25:03 +0400

    add index post author

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,27 +1,36 @@
-import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import User from '../users/user.entity';
 import Category from '../categories/category.entity';
- 
+
 @Entity()
 class Post {
   @PrimaryGeneratedColumn()
   public id: number;
- 
+
   @Column()
   public title: string;
- 
+
   @Column()
   public content: string;
- 
+
   @Column({ nullable: true })
   public category?: string;
- 
+
+  @Index('post_authorId_index')
   @ManyToOne(() => User, (author: User) => author.posts)
   public author: User;
- 
+
   @ManyToMany(() => Category)
   @JoinTable()
   public categories: Category[];
 }
- 
-export default Post
+
+export default Post;
